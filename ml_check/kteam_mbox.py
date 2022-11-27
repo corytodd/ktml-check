0e78994fb247739fb7a461940e0c5cafb9aa1b3f
Cory Todd <cory.todd@canonical.com>
2022-11-27 09:09:42 -0800

    kteam_mbox: check status code when fetching remote mail
    The http status code was not getting checked. This would lead to the
    creation of empty files on error that cannot be re-downloaded without
    using the clear_cache flag.
    
    Signed-off-by: Cory Todd <cory.todd@canonical.com>

--- ml_check/kteam_mbox.py
+++ ml_check/kteam_mbox.py
@@ -145,6 +145,10 @@ class KTeamMbox:
                 remote_file = config.MONTHLY_URL.format(year=year, month=month_name)
 
                 r = requests.get(remote_file)
+                if r.status_code != 200:
+                    logger.warning("failed to download %s.%s", year, month_name)
+                    continue
+
                 with open(cache_file, "wb") as f:
                     inflated = gzip.decompress(r.content)
                     f.write(inflated)
