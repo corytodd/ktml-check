fef0505edb0694a9dc2a5e0c06cc6d105b879ae7
Cory Todd <cory.todd@canonical.com>
2023-05-07 19:49:06 -0700

    setup: remove setup.py
    We no longer need setup.py for anything.
    
    Signed-off-by: Cory Todd <cory.todd@canonical.com>

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import find_namespace_packages, setup
-
-setup(
-    packages=find_namespace_packages(include=["ml_check"]),
-    include_package_data=True,
-    exclude_package_data={"": ["snap/*", "tests/*"]},
-)
